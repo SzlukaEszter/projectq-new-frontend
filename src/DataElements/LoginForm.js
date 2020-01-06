003f7dbb62ecec776a552c6893f7851dea521071
Eszter Szluka <szluka.eszter@gmail.com>
2020-01-06 14:48:16 +0100

    fix eventhandlers in login

--- src/DataElements/LoginForm.js
+++ src/DataElements/LoginForm.js
@@ -1,9 +1,13 @@
 import React, {Component} from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 class LoginForm extends Component {
-
+ state = {
+     username: null,
+     password: null
+ }
     getStyle() {
         return {
             background: 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))',
@@ -17,25 +21,32 @@ class LoginForm extends Component {
         return (
                 <div className="Formcontainer" style={this.getStyle()}>
                     <Form>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email"/>
+                        <Form.Group controlId="formBasicUsername">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="username" placeholder="Username" onChange={event => this.setState({username: event.target.value})}/>
                             <Form.Text className="text-muted">
                                 We'll never share your email with anyone else.
                             </Form.Text>
                         </Form.Group>
                         <Form.Group controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password"/>
+                            <Form.Control type="password" placeholder="Password" onChange={event => this.setState({password: event.target.value})}/>
                         </Form.Group>
-                        <Button variant="primary" type="submit">Sign In</Button><br/>
-                        <Button variant="secondary" type="submit" disabled>Sign Up</Button>
+                        <Button variant="primary" type="button" onClick={this.sendCredentials}>Sign In</Button><br/>
+                        <Button variant="secondary" type="button" disabled>Sign Up</Button>
                     </Form>
                 </div>
         );
     }
 
-
+    sendCredentials = () => {
+        axios.post("http://localhost:8080/auth/signin", {"username" :this.state.username, "password": this.state.password})
+            .then(
+                res => {
+                    console.log(res.data);
+                    document.cookie ="token="+ res.data.token;
+                });
+    }
 }
 
 export default LoginForm;
--- src/Views/LoginView.js
+++ src/Views/LoginView.js
@@ -4,15 +4,6 @@ import axios from 'axios';
 
 class LoginView extends Component {
 
-    state = {
-        token: null,
-        credentials: {
-            username: null,
-            password: null
-        }
-    };
-
-
     render() {
         return (
             <div className="App-container">
@@ -21,18 +12,7 @@ class LoginView extends Component {
         );
     }
 
-    sendCredentials = () => {
-        axios.post("http://localhost:8080/auth/signin", {"credentials" :this.state.credentials})
-            .then(
-            res => {
-                console.log(res.data);
-                this.setState(
-                    {
-                        token: res.data.token
-                    })
-            });
 
-    }
 }
 
 export default LoginView;
