1a0de64536c13a67d1a39d0b33b2f10ea077a994
Eszter Szluka <szluka.eszter@gmail.com>
2020-01-07 10:07:20 +0100

    log user data

--- src/DataElements/LoginForm.js
+++ src/DataElements/LoginForm.js
@@ -7,7 +7,7 @@ class LoginForm extends Component {
  state = {
      username: null,
      password: null
- }
+ };
     getStyle() {
         return {
             background: 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))',
@@ -23,14 +23,14 @@ class LoginForm extends Component {
                     <Form>
                         <Form.Group controlId="formBasicUsername">
                             <Form.Label>Username</Form.Label>
-                            <Form.Control type="username" placeholder="Username" onChange={event => this.setState({username: event.target.value})}/>
+                            <Form.Control type="username" placeholder="Username" onChange={event => this.setUn(event)}/>
                             <Form.Text className="text-muted">
                                 We'll never share your email with anyone else.
                             </Form.Text>
                         </Form.Group>
                         <Form.Group controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" onChange={event => this.setState({password: event.target.value})}/>
+                            <Form.Control type="password" placeholder="Password" onChange={event => this.setPw(event)}/>
                         </Form.Group>
                         <Button variant="primary" type="button" onClick={this.sendCredentials}>Sign In</Button><br/>
                         <Button variant="secondary" type="button" disabled>Sign Up</Button>
@@ -38,6 +38,16 @@ class LoginForm extends Component {
                 </div>
         );
     }
+
+    setPw = (e) => {
+        this.setState({password: e.target.value});
+        console.log(this.state);
+    };
+
+    setUn = (e) => {
+        this.setState({username: e.target.value});
+        console.log(this.state);
+    };
 
     sendCredentials = () => {
         axios.post("http://localhost:8080/auth/signin", {"username" :this.state.username, "password": this.state.password})
